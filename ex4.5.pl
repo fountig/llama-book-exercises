75095051f21d9cfab0c6f7857b3b26cbd164966c
George Fountis <118578531+fountig@users.noreply.github.com>
2023-06-18 10:37:56 +0300

    Add files via upload

--- ex4.5.pl
+++ ex4.5.pl
@@ -1,3 +1,46 @@
 #!/usr/bin/perl
 
-:
+# Modify the previous program to tell each new person the names of all the
+# people it has previously greeted:
+# greet( "Fred" );
+# greet( "Barney" );
+# greet( "Wilma" );
+# greet( "Betty" );
+#
+# This sequence of statements should print:
+#
+# Hi  Fred! You are the first one here!
+# Hi Barney! I've seen: Fred
+# Hi Wilma! I've seen: Fred Barney
+# Hi Betty! I've seen: Fred Barney Wilma
+use 5.010;
+
+sub greet {
+
+	state $person;
+	state @seen;
+
+	$person = $_[0]; 
+
+	if (scalar @seen eq 0) { 
+
+		print "Hi $person! You are the first one here!\n";
+	
+	}
+	
+	else { 	
+		print "Hi $person! @seen  is also here!\n";
+	}
+
+	push @seen, $person;
+}
+
+
+
+
+greet( "Fred"   );
+greet( "Barney" );
+greet( "Wilma"  );
+greet( "Betty"  );
+
+
